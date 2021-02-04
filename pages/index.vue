4686d090cdaa04dfd570cf5caecc25c8b04e4397
zainrax <patrickb321@gmail.com>
2021-02-04 15:49:30 +1300

    feat: initial nuxt.js & tailwind setup

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <h1 class="h">Hello</h1>
+    <NuxtLink to="/test">Test</NuxtLink>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+@apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+</style>
